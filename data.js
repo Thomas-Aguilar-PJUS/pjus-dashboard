75de72d10da095660e54e9b01a4f57bc79dd1445
Thomas Borges Aguilar <thomas.aguilar@pjus.com.br>
2026-03-20 12:36:33 -0300

    Atualização automática - 20/03/2026 12:36

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 // Dados extraídos automaticamente - Projeto Acelera Comercial
-// Última atualização: 20/03/2026 12:34
+// Última atualização: 20/03/2026 12:36
 // Fonte: Base Controle Operacional e Produção Anual.xlsx
 
 const ET=["Assinatura", "Avaliação do ativo", "Confecção de documentos", "Conferência final", "Conferência inicial", "Documentos preliminares", "Homologação", "Operações pagas", "Operações reprovadas", "Pagamento", "Parecer do titular", "Parecer jurídico", "Protocolo de cessão", "Providências jurídicas"];
